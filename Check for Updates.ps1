b35ed6671665d46c9a9578f0872eb243d5f8ece5
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-05-02 16:38:41 -0500

    Update Check for Updates.ps1

--- Check for Updates.ps1
+++ Check for Updates.ps1
@@ -1,35 +1,90 @@
-    If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+#Made by Noah Lawson
+
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        # Relaunch as an elevated process:
+        Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+        exit
+    }
+
+function Get-WindowsUpdateModuleInstallationStatus
 {
-    # Relaunch as an elevated process:
-    Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
-    exit
+    #If statement to check if PSWindowsUpdate is already installed.
+    $InstalledModule = Get-InstalledModule -Name PSWindowsUpdate
+
+if ($InstalledModule = "PSWindowsUpdate")
+{
+    Get-UpdateHistory
+    Get-CurrentAvailableUpdates
+}
+else
+{
+    $Count = 0
+
+    Get-WindowsUpdateModule
+
+    $Count++
+
+    If ($Count = 4)
+    {
+        write-host "This is not working. Sorry!"
+        exit
+    }
+
+    sleep 5
+
+    Get-WindowsUpdateModuleInstallationStatus
+}
 }
 
-Set-ExecutionPolicy Bypass
+function Get-WindowsUpdateModule
+{
+    #Install correct modules to show available updates
+    Install-Module -Name PSWindowsUpdate -force
+    Import-Module -Name PSWindowsUpdate
+}
 
+function Get-UpdateHistory
+{
 #Display past updates that have been installed
 get-wmiobject -class win32_quickfixengineering | Sort-Object InstalledOn | Format-Table Description, HotFixID, InstalledOn
 
 write-host "^^^^ Most recently installed updates are listed above ^^^^"
+}
 
-#If statement to check if PSWindowsUpdate is already installed.
-$InstalledModule = Get-InstalledModule -Name PSWindowsUpdate
-
-if ($InstalledModule = "PSWindowsUpdate")
+function Get-CurrentAvailableUpdates
 {
 #Display Windows Updates that are available
-Get-WindowsUpdate | Format-Table HotFixID, Title
+Get-WUServiceManager | Format-Table Name, ServiceID
 
 write-host "^^^^ Updates that can be installed are listed above ^^^^"
 }
-else
+
+function Get-UserInputForUpdates
 {
-#Install correct modules to show available updates
-Install-Module -Name PSWindowsUpdate -force
-Import-Module -Name PSWindowsUpdate
+    write-host 
 
-#Display Windows Updates that are available
-Get-WindowsUpdate | Format-Table HotFixID, Title
+    $WouldYouLikeToUpdate = read-host -prompt "Would you like to install all currently available updates? Enter yes or no"
 
-write-host "^^^^ Updates that can be installed are listed above ^^^^"
+    if ($WouldYouLikeToUpdate -eq "yes")
+    {
+        Install-WindowsUpdate -AcceptAll -AutoReboot
+    }
+    else
+    {
+        write-host "Goodbye!"
+        sleep 5
+        exit
+    }
+
 }
+
+Set-ExecutionPolicy Bypass
+Get-WindowsUpdateModuleInstallationStatus
+Get-UserInputForUpdates
+
+
+
+
+
+
